a04940492e3097c769b6cf0a02144105774af250
Drew Miley <drewmiley92@gmail.com>
2021-01-20 23:59:31 +0000

    Initial work on Schemas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB || 'mongodb://localhost/test');
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost/blackjacks');
 const Schema = mongoose.Schema;
 
 const express = require('express');
@@ -20,6 +20,66 @@ router.use((req, res, next) => {
 router.get('/', (req, res) => {
     res.json({ message: 'blackjacks backend is running' });
 });
+
+router.post('/init', (req, res) => {
+    // Add Game to DB
+    console.log(req.body.players);
+    res.json({ message: req.body.players });
+})
+
+router.get('/state/:player', (req, res) => {
+    // Get Game Data from DB as views by Player
+    console.log(req.params.player);
+    res.json({ message: `returning state for ${req.params.player}` });
+})
+
+router.post('/play/:player', (req, res) => {
+    // Validate whether valid set of cards, and update game situation in DB
+    // Return /state/:player response
+    console.log(req.params.player);
+    console.log(req.body.cards);
+    res.json({ message: `playing ${req.body.cards} cards for ${req.params.player}` });
+})
+
+const CARD_VALUES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+const SUITS = [
+    { name: 'Clubs', isBlack: true },
+    { name: 'Spades', isBlack: true },
+    { name: 'Hearts', isBlack: false },
+    { name: 'Diamonds', isBlack: false }
+]
+
+const CardSchema = new Schema({
+    value: String,
+    suit: String
+});
+// const Card = mongoose.model('Card', CardSchema);
+
+const PlayerSchema = new Schema({
+    name: String,
+    hand: [CardSchema]
+});
+// const Player = mongoose.model('Player', PlayerSchema);
+
+// TODO: Create JacksTwoAndEightsActiveSchema
+const BlackJacksActiveCardsSchema = new Schema({
+    value: String,
+    suit: String,
+    king: Boolean,
+    two: Number,
+    blackjacks: Number
+});
+// const BlackJacksActiveCards = mongoose.model('BlackJacksActiveCards', BlackJacksActiveCardsSchema);
+
+const GameSchema = new Schema({
+    deck: [CardSchema],
+    lastCardsPlayed: [CardSchema],
+    players: [PlayerSchema],
+    turn: String,
+    activeCards: BlackJacksActiveCardsSchema
+});
+const Deck = mongoose.model('Deck', DeckSchema);
+
 
 // const QuizSchema = new Schema({
 //     code: String,
