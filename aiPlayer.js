3f34bd5d6c0aaa4c10447857b009a24fe5ccc990
Drew Miley <drewmiley92@gmail.com>
2021-09-06 19:57:32 +0100

    Copies in old java code

--- aiPlayer.js
+++ aiPlayer.js
@@ -1,5 +1,180 @@
+// package players;
+//
+// import gamemodel.*;
+//
+// import java.util.ArrayList;
+// import java.util.Collection;
+// import java.util.List;
+// import java.util.stream.Collectors;
+// import java.util.stream.IntStream;
+// import java.util.stream.Stream;
+//
+// public abstract class Player {
+//     private int playerIndex;
+//     List<Card> hand;
+//     List<Card> knownHand = new ArrayList<>();
+//
+//     Player(int playerIndex, List<Card> hand) {
+//         this.playerIndex = playerIndex;
+//         this.hand = hand;
+//     }
+//
+//     List<List<Card>> possibleCardsToPlay(Rules rules, Pile pile) {
+//         List<List<Card>> possibleCardsToPlay = new ArrayList<>();
+//         final List<List<Card>>[] result = new List[]{new ArrayList<>()};
+//         result[0] = hand.stream()
+//                 .map(card -> Stream.of(card).collect(Collectors.toList()))
+//                 .filter(card -> rules.isAllowedPlay(pile, card))
+//                 .collect(Collectors.toList());
+//         possibleCardsToPlay.addAll(result[0]);
+//         IntStream.range(0, hand.size() - 1)
+//                 .mapToObj(i -> hand)
+//                 .flatMap(Collection::stream)
+//                 .forEach(handCard -> {
+//                     result[0] = result[0].stream()
+//                             .filter(cards -> {
+//                                 List<Card> cardPair = Stream.of(cards.get(cards.size() - 1), handCard)
+//                                         .collect(Collectors.toList());
+//                                 return !cards.contains(handCard) && rules.runValid(cardPair, pile);
+//                             })
+//                             .map(cards -> Stream.concat(
+//                                             cards.stream(),
+//                                             Stream.of(handCard).collect(Collectors.toList()).stream()
+//                                     ).collect(Collectors.toList()))
+//                             .collect(Collectors.toList());
+//                     possibleCardsToPlay.addAll(result[0]);
+//                 });
+//         possibleCardsToPlay.add(new ArrayList<>());
+//         return possibleCardsToPlay;
+//     }
+//
+//     public abstract List<Card> cardsToPlay(Rules rules, Deck deck, Pile pile, List<VisiblePlayer> visiblePlayers);
+//
+//     public abstract Suit nomination(Rules rules, Deck deck, Pile pile, List<VisiblePlayer> visiblePlayers);
+//
+//     public void addCardToHand(Card card, boolean deckOrderSeen) {
+//         this.hand.add(card);
+//         if (deckOrderSeen) this.knownHand.add(card);
+//     }
+//
+//     public void removeCardFromHand(Card card) {
+//         this.hand.remove(card);
+//         this.knownHand.remove(card);
+//     }
+//
+//     public boolean handEmpty() {
+//         return this.hand.isEmpty();
+//     }
+//
+//     public abstract PlayerType getPlayerType();
+// }
+
+// package players;
+//
+// import gamemodel.*;
+//
+// import java.util.*;
+// import java.util.stream.Collectors;
+// import java.util.stream.Stream;
+//
+// class ProportionalPlayer extends Player {
+//
+//     ProportionalPlayer(int playerIndex, List<Card> hand) {
+//         super(playerIndex, hand);
+//     }
+//
+//     @Override
+//     public List<Card> cardsToPlay(Rules rules, Deck deck, Pile pile, List<VisiblePlayer> visiblePlayers) {
+//         List<List<Card>> possibleCardsToPlay = this.possibleCardsToPlay(rules, pile);
+//         List<Card> unplayedCardsRemainingInGame = unplayedCardsRemainingInGame(pile, hand);
+//         Long maxCardsToPlaySize = possibleCardsToPlay.stream()
+//                 .mapToLong(List::size)
+//                 .max()
+//                 .getAsLong();
+//         if (maxCardsToPlaySize == 0) {
+//             return new ArrayList<>();
+//         }
+//         Map<List<Card>, Double> maxSuitProportions = possibleCardsToPlay.stream()
+//                 .filter(cards -> cards.size() == maxCardsToPlaySize)
+//                 .collect(Collectors.toMap(c -> c,
+//                     c -> {
+//                         Map<Suit, Double> suitProportions = suitProportionsAfterCardsPlayed(
+//                                 unplayedCardsRemainingInGame,
+//                                 hand,
+//                                 c,
+//                                 Arrays.asList(FaceValue.TWO, FaceValue.JACK)
+//                         );
+//                         return c.get(c.size() - 1).getFaceValue() == rules.NOMINATE_SUIT ?
+//                                 suitProportions.entrySet().stream()
+//                                     .min(Comparator.comparingDouble(Map.Entry::getValue))
+//                                     .get()
+//                                     .getValue() :
+//                                 suitProportions.get(c.get(c.size() - 1).getSuit());
+//
+//                     })
+//                 );
+//         return maxSuitProportions.entrySet().stream()
+//                 .max(Comparator.comparingDouble(Map.Entry::getValue))
+//                 .get()
+//                 .getKey();
+//     }
+//
+//     @Override
+//     public Suit nomination(Rules rules, Deck deck, Pile pile, List<VisiblePlayer> visiblePlayers) {
+//         List<Card> unplayedCardsRemainingInGame = unplayedCardsRemainingInGame(pile, hand);
+//         Map<Suit, Double> suitProportions = suitProportionsAfterCardsPlayed(
+//                 unplayedCardsRemainingInGame,
+//                 hand,
+//                 new ArrayList<>(),
+//                 Collections.singletonList(FaceValue.JACK)
+//         );
+//         return suitProportions.entrySet().stream()
+//                 .max(Comparator.comparingDouble(Map.Entry::getValue))
+//                 .get()
+//                 .getKey();
+//     }
+//
+//     private List<Card> unplayedCardsRemainingInGame(Pile pile, List<Card> hand) {
+//         List<Card> cards = Stream.of(FaceValue.values())
+//                 .flatMap(faceValue -> Stream.of(Suit.values()).map(suit -> new Card(faceValue, suit)))
+//                 .collect(Collectors.toList());
+//         cards.remove(pile.topCard());
+//         cards.removeAll(pile.getCardsBelowTopCard());
+//         cards.removeAll(hand);
+//         return cards;
+//     }
+//
+//     private Map<Suit, Double> suitProportionsAfterCardsPlayed(List<Card> unplayedCardsRemainingInGame,
+//                                                             List<Card> hand,
+//                                                             List<Card> cardsToPlay,
+//                                                             List<FaceValue> faceValuesToIgnore) {
+//         List<Card> unPlayedCardsIgnoringFaceValues = unplayedCardsRemainingInGame.stream()
+//                 .filter(card -> !faceValuesToIgnore.contains(card.getFaceValue()))
+//                 .collect(Collectors.toList());
+//         return Arrays.stream(Suit.values())
+//                 .collect(Collectors.toMap(s -> s,
+//                         s -> {
+//                             Double suitLeftInHand = (double) hand.stream()
+//                                     .filter(card -> !faceValuesToIgnore.contains(card.getFaceValue()) && !cardsToPlay.contains(card))
+//                                     .filter(card -> card.getSuit() == s)
+//                                     .count();
+//                             Double suitLeftInGame = (double) unPlayedCardsIgnoringFaceValues.stream()
+//                                     .filter(card -> card.getSuit() == s)
+//                                     .count();
+//                             return suitLeftInHand / suitLeftInGame;
+//                         })
+//                 );
+//     }
+//
+//     @Override
+//     public PlayerType getPlayerType() {
+//         return PlayerType.PROPORTIONAL_PLAYER;
+//     }
+//
+// }
+
 const playCards = gameState => {
-    return null;
+    return { cards: null, nomination: null };
 }
 
 const aiPlayer = { playCards };
